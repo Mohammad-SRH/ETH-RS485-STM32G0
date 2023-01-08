808a8d1acc452f59e90799198fea0e0fe401ee1b
MohammadSRH <mohammad.sr2007@gmail.com>
2023-01-08 13:14:29 +0330

    Initial Version 011018

---
+++ MDK-ARM/RTE/_ETH-RS485-STM32G0/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ETH-RS485-STM32G0' 
+ * Target:  'ETH-RS485-STM32G0' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32g0xx.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
